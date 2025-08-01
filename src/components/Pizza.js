a846048571f6bb44f27426944064fe9ac23d2df9
furtadomn <mnfurtado@yahoo.com.br>
2025-08-01 10:47:02 -0300

    improve Pizza component

--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -1,19 +1,7 @@
-import data from "./../data/pizzaData.js";
+import Menu from "./Menu.js";
 
 function Pizza() {
-  const pizzas = data.map((pizza) => {
-    return (
-      <div>
-        <h2>{pizza.name}</h2>
-        <img src={pizza.photoName} alt={pizza.name} />
-        <p>{pizza.ingredients}</p>
-        <p>{pizza.price}</p>
-        <p>{pizza.soldOut}</p>
-      </div>
-    );
-  });
-
-  return <div>{pizzas}</div>;
+  return <Menu />;
 }
 
 export default Pizza;
