ff393c398503d39a9ebdbd8468373e26805399fd
furtadomn <mnfurtado@yahoo.com.br>
2025-08-09 15:56:40 -0300

    use conditional rendering with ternaries

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,15 +4,18 @@ function Footer() {
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
 
-  if (isOpen) {
-    alert("We're currently open!");
-  } else {
-    alert("Sorry we're closed");
-  }
-
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()}. We're currently open!
+      {isOpen ? (
+        <div className="order">
+          <p>We're open until {closeHour}:00. Come visit us or order online!</p>
+          <button className="btn">Order</button>
+        </div>
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
+      )}
     </footer>
   );
 }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,15 +1,21 @@
 import Pizza from "./Pizza.js";
-import data from "./../data/pizzaData";
+import pizzasData from "./../data/pizzaData";
+
+const numPizzas = pizzasData.length;
 
 function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <ul className="pizzas">
-        {data.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
+      {numPizzas > 0 ? (
+        <ul className="pizzas">
+          {pizzasData.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      ) : (
+        <p>We're still working on our menu. Please come back later.</p>
+      )}
     </main>
   );
 }
